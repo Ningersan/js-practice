e97bbd856f06ee3234b24f952f962206b01cc69c
zining <ningersan@outlook.com>
2017-12-12 22:11:07 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+一些关于js的小练习，包括一个轮播图，一个简单的vue模板引擎。
 carousel demo: https://htmlpreview.github.io/?https://github.com/Ningersan/js-practice/blob/master/banner/banner.html
--- my-react/src/components/Slider/index.jsx
+++ my-react/src/components/Slider/index.jsx
@@ -5,14 +5,51 @@ import './index.css'
 class Slider extends Component {
     constructor() {
         super()
+        this.state = {
+            offset: 0,
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    tick(offset) {
+        this.setState((prevState) => ({
+            offset: prevState.offset + offset
+        }))
+    }
+
+    slide() {
+        const { offset } = this.state
+        if (offset <= 0 && offset > -400) {
+            this.tick(-100)
+        } else if (offset <= -400) {
+            this.tick(400)
+        } else {
+            this.tick(-400)
+        }
+    }
+
+    handleClick(e) {
+        var offset = {'to-left': 100, 'to-right': -100}
+        this.tick(offset[e.target.className])
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(() => this.slide(), 2000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer)
     }
 
     render() {
+        var listStyle = { left: `${this.state.offset}%` };
         return (
             <div id="slider">
-                <div className="img-list">
+                <div className="img-list" style={listStyle}>
                     {this.props.children}
                 </div>
+                <button className='to-left' onClick={this.handleClick}></button>
+                <button className='to-right' onClick={this.handleClick}></button>
             </div>
         )
     }
